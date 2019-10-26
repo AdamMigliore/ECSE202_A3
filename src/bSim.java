557d70353036928b48afda1d873dd07323d79769
AdamMigliore <33816654+AdamMigliore@users.noreply.github.com>
2019-10-25 20:51:48 -0400

    Update bSim.java

--- src/bSim.java
+++ src/bSim.java
@@ -1,6 +1,7 @@
 
 import java.awt.Color;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import acm.graphics.GLabel;
 import acm.graphics.GOval;
@@ -9,8 +10,7 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 /**
- * @author adamd
- * Assignment 3 for McGill Fall19 ECSE202 class
+ * @author adamd Assignment 3 for McGill Fall19 ECSE202 class
  */
 public class bSim extends GraphicsProgram {
 
@@ -18,7 +18,7 @@ public class bSim extends GraphicsProgram {
 	private static final int HEIGHT = 600; // Height of window
 	private static final int OFFSET = 200; // offset for plane
 	private static final double SCALE = HEIGHT / 100; // pixels per meter
-	private static final int NUMBALLS = 10; // # balls to simulate
+	private static final int NUMBALLS = 60; // # balls to simulate
 	private static final double MINSIZE = 1.0; // Minumum ball radius (meters)
 	private static final double MAXSIZE = 7.0; // Maximum ball radius (meters)
 	private static final double EMIN = 0.2; // Minimum loss coefficient
@@ -28,46 +28,54 @@ public class bSim extends GraphicsProgram {
 	private static final double ThetaMIN = 80.0; // Minimum launch angle (degrees)
 	private static final double ThetaMAX = 100.0; // Maximum launch angle (degrees)
 	private static final int GP_HEIGHT = 3; // Maximum launch angle (degrees)
-	private RandomGenerator rgen = new RandomGenerator(); //randomGenerator 
-	private bTree myTree = new bTree();
-	private GLabel message;
+	private RandomGenerator rgen = new RandomGenerator(); // randomGenerator
+	private bTree myTree = new bTree();//our bTree
+	private GLabel message;//label object for the display
+	private boolean mouseListening=true;//true if the user can stack balls, if not it is false
+	
 	/**
-	 * the main loop of the simulation which will generate the random parameters for the number of balls in the simulation
+	 * the main loop of the simulation which will generate the random parameters for
+	 * the number of balls in the simulation
 	 */
 	public void run() {
-		
-		rgen.setSeed((long) 424242);//required from prof. to match simulation parameters
-		
+
+		rgen.setSeed((long) 424242);// required from prof. to match simulation parameters
+
 		setupDisplay();
-		
+
 		for (int i = 0; i < NUMBALLS; i++) {
 
-			double bSize = rgen.nextDouble(MINSIZE, MAXSIZE); //randomly generate ball size element of [MINSIZE,MAXSIZE]
-			Color bColor = rgen.nextColor();//randomly generate color
-			double bLoss = rgen.nextDouble(EMIN, EMAX);//randomly generate energy loss element of [EMIN,EMAX]
-			double Vo = rgen.nextDouble(VoMIN, VoMAX); //randomly generate initial velocity element of [VoMIN,VoMAX]
-			double theta = rgen.nextDouble(ThetaMIN, ThetaMAX); //randomly generate theta element of [ThetaMIN,ThetaMAX]
+			double bSize = rgen.nextDouble(MINSIZE, MAXSIZE); // randomly generate ball size element of
+																// [MINSIZE,MAXSIZE]
+			Color bColor = rgen.nextColor();// randomly generate color
+			double bLoss = rgen.nextDouble(EMIN, EMAX);// randomly generate energy loss element of [EMIN,EMAX]
+			double Vo = rgen.nextDouble(VoMIN, VoMAX); // randomly generate initial velocity element of [VoMIN,VoMAX]
+			double theta = rgen.nextDouble(ThetaMIN, ThetaMAX); // randomly generate theta element of
+																// [ThetaMIN,ThetaMAX]
 
 			aBall ball = new aBall(gUtil.pixelsToMeter(SCALE, WIDTH / 2), bSize, Vo, theta, bSize, bColor, bLoss, SCALE,
-					WIDTH, HEIGHT);//Generate a new aBall object with it's initial parameters
-			add(ball.getBall());//add the ball to the simulation
+					WIDTH, HEIGHT);// Generate a new aBall object with it's initial parameters
+			add(ball.getBall());// add the ball to the simulation
 			myTree.addNode(ball);
-			ball.start();//start the balls thread
+			ball.start();// start the balls thread
 
 		}
-		
-		while (myTree.isRunning()) {//not exactly
+
+		while (myTree.isRunning()) {//avoids running the rest of the program until all balls are done simulating
 			myTree.checkRunning();
 		}
-		
-		message = new GLabel("CR to continue", WIDTH-100,HEIGHT-100);
+
+		message = new GLabel("Click mouse to continue");
+		message.setLocation(WIDTH - message.getWidth(), HEIGHT - message.getHeight());
 		message.setColor(Color.RED);
 		add(message);
+
 		addMouseListeners();
 	}
 
 	/**
-	 * Set up the simulation display: create and add the plane to the simulation and resize the window
+	 * Set up the simulation display: create and add the plane to the simulation and
+	 * resize the window
 	 */
 	private void setupDisplay() {
 		this.resize(WIDTH, HEIGHT + OFFSET);
@@ -78,15 +86,29 @@ public class bSim extends GraphicsProgram {
 		add(plane);
 	}
 	
+	/**
+	 * event that happens when the mouse is pressed; stack the balls and change the displayed message
+	 */
 	public void mousePressed(MouseEvent e) {
-		myTree.stackBalls(this);
-		message.setLabel("All Stacked");
+		if (mouseListening) {
+			myTree.stackBalls(this);
+			message.setLabel("All Stacked");
+			mouseListening=!mouseListening;
+		}
+
 	}
+
 	
+	/**
+	 * @return double scale : scale used in the simulation
+	 */
 	public double getScale() {
 		return SCALE;
 	}
-	
+
+	/**
+	 * @return int height : height of the window
+	 */
 	public int getHeight() {
 		return HEIGHT;
 	}
